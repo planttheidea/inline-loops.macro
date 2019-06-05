391f6c9c656df6281133b218fc198602cca2653b
planttheidea <tony.quetano@planttheidea.com>
2019-06-05 06:37:48 -0700

    round out build setup

--- index.d.ts
+++ index.d.ts
@@ -1,56 +1,66 @@
-type Dictionary<T> = {
-  [key: string]: T;
-  [index: number]: T;
+type Dictionary<Type> = {
+  [key: string]: Type;
+  [index: number]: Type;
 };
 
+/**
+ * iterables
+ */
 type ArrayIterable = any[];
 type ObjectIterable = Dictionary<any>;
 
+/**
+ * the handlers for the iterables, specific to type
+ */
 type ArrayHandler = (value?: any[], index?: number, array?: ArrayIterable) => any;
 type ObjectHandler = (value?: any[], key?: string, object?: ObjectIterable) => any;
 
-export function every(iterable: ArrayIterable, handler: ArrayHandler): boolean;
-export function everyObject(iterable: ObjectIterable, handler: ObjectHandler): boolean;
-export function everyRight(iterable: ArrayIterable, handler: ArrayHandler): boolean;
+/**
+ * available exports
+ */
 
-export function filter<Iterable = ArrayIterable>(
+declare function every(iterable: ArrayIterable, handler: ArrayHandler): boolean;
+declare function everyObject(iterable: ObjectIterable, handler: ObjectHandler): boolean;
+declare function everyRight(iterable: ArrayIterable, handler: ArrayHandler): boolean;
+
+declare function filter<Iterable = ArrayIterable>(
   iterable: Iterable,
   handler: ArrayHandler,
 ): Iterable;
-export function filterObject<Iterable = ObjectIterable>(
+declare function filterObject<Iterable = ObjectIterable>(
   iterable: ObjectIterable,
   handler: ObjectHandler,
 ): Iterable;
-export function filterRight<Iterable = ArrayIterable>(
+declare function filterRight<Iterable = ArrayIterable>(
   iterable: Iterable,
   handler: ArrayHandler,
 ): Iterable;
 
-export function find(iterable: ArrayIterable, handler: ArrayHandler): any;
-export function findObject(iterable: ObjectIterable, handler: ObjectHandler): any;
-export function findRight(iterable: ArrayIterable, handler: ArrayHandler): any;
+declare function find(iterable: ArrayIterable, handler: ArrayHandler): any;
+declare function findObject(iterable: ObjectIterable, handler: ObjectHandler): any;
+declare function findRight(iterable: ArrayIterable, handler: ArrayHandler): any;
 
-export function findIndex<Iterable = ArrayIterable>(
+declare function findIndex<Iterable = ArrayIterable>(
   iterable: Iterable,
   handler: ArrayHandler,
 ): keyof Iterable | -1;
-export function findIndexRight<Iterable = ArrayIterable>(
+declare function findIndexRight<Iterable = ArrayIterable>(
   iterable: Iterable,
   handler: ArrayHandler,
 ): keyof Iterable | -1;
 
-export function findKey<Iterable = ObjectIterable>(
+declare function findKey<Iterable = ObjectIterable>(
   iterable: Iterable,
   handler: ObjectHandler,
 ): keyof Iterable | void;
 
-export function forEach(iterable: ArrayIterable, handler: ArrayHandler): void;
-export function forEachObject(iterable: ObjectIterable, handler: ObjectHandler): void;
-export function forEachRight(iterable: ArrayIterable, handler: ArrayHandler): void;
+declare function forEach(iterable: ArrayIterable, handler: ArrayHandler): void;
+declare function forEachObject(iterable: ObjectIterable, handler: ObjectHandler): void;
+declare function forEachRight(iterable: ArrayIterable, handler: ArrayHandler): void;
 
-export function map(iterable: ArrayIterable, handler: ArrayHandler): ArrayIterable;
-export function mapObject(iterable: ObjectIterable, handler: ObjectHandler): ObjectIterable;
-export function mapRight(iterable: ArrayIterable, handler: ArrayHandler): ArrayIterable;
+declare function map(iterable: ArrayIterable, handler: ArrayHandler): ArrayIterable;
+declare function mapObject(iterable: ObjectIterable, handler: ObjectHandler): ObjectIterable;
+declare function mapRight(iterable: ArrayIterable, handler: ArrayHandler): ArrayIterable;
 
 type ArrayReduceHandler = (
   accumulator?: any,
@@ -65,22 +75,49 @@ type ObjectReduceHandler = (
   object?: ObjectIterable,
 ) => any;
 
-export function reduce(
+declare function reduce(
   iterable: ArrayIterable,
   handler: ArrayReduceHandler,
   initialValue?: any,
 ): any;
-export function reduceObject(
+declare function reduceObject(
   iterable: ObjectIterable,
   handler: ObjectReduceHandler,
   initialValue?: any,
 ): any;
-export function reduceRight(
+declare function reduceRight(
   iterable: ArrayIterable,
   handler: ArrayReduceHandler,
   initialValue?: any,
 ): any;
 
-export function some(iterable: ArrayIterable, handler: ArrayHandler): boolean;
-export function someObject(iterable: ObjectIterable, handler: ObjectHandler): boolean;
-export function someRight(iterable: ArrayIterable, handler: ArrayHandler): boolean;
+declare function some(iterable: ArrayIterable, handler: ArrayHandler): boolean;
+declare function someObject(iterable: ObjectIterable, handler: ObjectHandler): boolean;
+declare function someRight(iterable: ArrayIterable, handler: ArrayHandler): boolean;
+
+export {
+  every,
+  everyObject,
+  everyRight,
+  filter,
+  filterObject,
+  filterRight,
+  find,
+  findIndex,
+  findIndexRight,
+  findKey,
+  findObject,
+  findRight,
+  forEach,
+  forEachObject,
+  forEachRight,
+  map,
+  mapObject,
+  mapRight,
+  reduce,
+  reduceObject,
+  reduceRight,
+  some,
+  someObject,
+  someRight,
+};
