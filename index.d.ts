cdd912b30bf65d3a180912bb030faad8bf6e345f
planttheidea <tony.quetano@planttheidea.com>
2019-10-19 21:35:53 -0700

    update definitions file

--- index.d.ts
+++ index.d.ts
@@ -6,94 +6,137 @@ type Dictionary<Type> = {
 /**
  * iterables
  */
-type ArrayIterable = any[];
-type ObjectIterable = Dictionary<any>;
+type ArrayIterable<Type extends any = any> = Type[];
+type ObjectIterable<Type extends any = any> = Dictionary<Type>;
 
 /**
  * the handlers for the iterables, specific to type
  */
-type ArrayHandler = (value: any, index: number, array: ArrayIterable) => any;
-type ObjectHandler = (value: any, key: string, object: ObjectIterable) => any;
+type ArrayHandler<Type extends any = any> = (
+  value: Type,
+  index: number,
+  array: ArrayIterable<Type>,
+) => any;
+type ObjectHandler<Type extends any = any> = (
+  value: Type,
+  key: string,
+  object: ObjectIterable<Type>,
+) => any;
 
 /**
  * available exports
  */
 
-declare function every(iterable: ArrayIterable, handler: ArrayHandler): boolean;
-declare function everyObject(iterable: ObjectIterable, handler: ObjectHandler): boolean;
-declare function everyRight(iterable: ArrayIterable, handler: ArrayHandler): boolean;
+declare function every<Type>(iterable: ArrayIterable<Type>, handler: ArrayHandler<Type>): boolean;
+declare function everyObject<Type>(
+  iterable: ObjectIterable<Type>,
+  handler: ObjectHandler<Type>,
+): boolean;
+declare function everyRight<Type>(
+  iterable: ArrayIterable<Type>,
+  handler: ArrayHandler<Type>,
+): boolean;
 
-declare function filter<Iterable = ArrayIterable>(
-  iterable: Iterable,
-  handler: ArrayHandler,
-): Iterable;
-declare function filterObject<Iterable = ObjectIterable>(
-  iterable: ObjectIterable,
-  handler: ObjectHandler,
-): Iterable;
-declare function filterRight<Iterable = ArrayIterable>(
-  iterable: Iterable,
-  handler: ArrayHandler,
-): Iterable;
+declare function filter<Type>(
+  iterable: ArrayIterable<Type>,
+  handler: ArrayHandler<Type>,
+): ArrayIterable<Type>;
+declare function filterObject<Type>(
+  iterable: ObjectIterable<Type>,
+  handler: ObjectHandler<Type>,
+): ObjectIterable<Type>;
+declare function filterRight<Type>(
+  iterable: ArrayIterable<Type>,
+  handler: ArrayHandler<Type>,
+): ArrayIterable<Type>;
 
-declare function find(iterable: ArrayIterable, handler: ArrayHandler): any;
-declare function findObject(iterable: ObjectIterable, handler: ObjectHandler): any;
-declare function findRight(iterable: ArrayIterable, handler: ArrayHandler): any;
+declare function find<Type extends any = any>(
+  iterable: ArrayIterable<Type>,
+  handler: ArrayHandler<Type>,
+): Type | undefined;
+declare function findObject<Type extends any = any>(
+  iterable: ObjectIterable<Type>,
+  handler: ObjectHandler<Type>,
+): Type | undefined;
+declare function findRight<Type extends any = any>(
+  iterable: ArrayIterable<Type>,
+  handler: ArrayHandler<Type>,
+): Type | undefined;
 
-declare function findIndex<Iterable = ArrayIterable>(
+declare function findIndex<Type, Iterable extends ArrayIterable<Type> = ArrayIterable<Type>>(
   iterable: Iterable,
-  handler: ArrayHandler,
+  handler: ArrayHandler<Type>,
 ): keyof Iterable | -1;
-declare function findIndexRight<Iterable = ArrayIterable>(
+declare function findIndexRight<Type, Iterable extends ArrayIterable<Type> = ArrayIterable<Type>>(
   iterable: Iterable,
-  handler: ArrayHandler,
+  handler: ArrayHandler<Type>,
 ): keyof Iterable | -1;
-
-declare function findKey<Iterable = ObjectIterable>(
+declare function findKey<Type, Iterable extends ObjectIterable<Type> = ObjectIterable<Type>>(
   iterable: Iterable,
-  handler: ObjectHandler,
+  handler: ObjectHandler<Type>,
 ): keyof Iterable | void;
 
-declare function forEach(iterable: ArrayIterable, handler: ArrayHandler): void;
-declare function forEachObject(iterable: ObjectIterable, handler: ObjectHandler): void;
-declare function forEachRight(iterable: ArrayIterable, handler: ArrayHandler): void;
+declare function forEach<Type>(iterable: ArrayIterable<Type>, handler: ArrayHandler<Type>): void;
+declare function forEachObject<Type>(
+  iterable: ObjectIterable<Type>,
+  handler: ObjectHandler<Type>,
+): void;
+declare function forEachRight<Type>(
+  iterable: ArrayIterable<Type>,
+  handler: ArrayHandler<Type>,
+): void;
 
-declare function map(iterable: ArrayIterable, handler: ArrayHandler): ArrayIterable;
-declare function mapObject(iterable: ObjectIterable, handler: ObjectHandler): ObjectIterable;
-declare function mapRight(iterable: ArrayIterable, handler: ArrayHandler): ArrayIterable;
+declare function map<Type extends any = any, NewType extends any = Type>(
+  iterable: ArrayIterable<Type>,
+  handler: ArrayHandler<Type>,
+): ArrayIterable<NewType>;
+declare function mapObject<Type extends any = any, NewType extends any = Type>(
+  iterable: ObjectIterable<Type>,
+  handler: ObjectHandler<Type>,
+): ObjectIterable<NewType>;
+declare function mapRight<Type extends any = any, NewType extends any = Type>(
+  iterable: ArrayIterable<Type>,
+  handler: ArrayHandler<Type>,
+): ArrayIterable<NewType>;
 
-type ArrayReduceHandler = (
-  accumulator?: any,
-  value?: any,
-  index?: number,
-  array?: ArrayIterable,
-) => any;
-type ObjectReduceHandler = (
-  accumulator?: any,
-  value?: any,
-  key?: string,
-  object?: ObjectIterable,
-) => any;
+type ArrayReduceHandler<Type extends any = any, Result extends any = any> = (
+  result: Result,
+  value: Type,
+  index: number,
+  array: ArrayIterable<Type>,
+) => Result;
+type ObjectReduceHandler<Type extends any = any, Result extends any = any> = (
+  accumulator: Result,
+  value: Type,
+  key: string,
+  object: ObjectIterable<Type>,
+) => Result;
 
-declare function reduce(
-  iterable: ArrayIterable,
-  handler: ArrayReduceHandler,
-  initialValue?: any,
-): any;
-declare function reduceObject(
-  iterable: ObjectIterable,
-  handler: ObjectReduceHandler,
-  initialValue?: any,
-): any;
-declare function reduceRight(
-  iterable: ArrayIterable,
-  handler: ArrayReduceHandler,
-  initialValue?: any,
-): any;
+declare function reduce<Type, Result extends any = any, InitialValue extends any = any>(
+  iterable: ArrayIterable<Type>,
+  handler: ArrayReduceHandler<Type, Result>,
+  initialValue?: InitialValue,
+): Result;
+declare function reduceObject<Type, Result extends any = any, InitialValue extends any = any>(
+  iterable: ObjectIterable<Type>,
+  handler: ObjectReduceHandler<Type, Result>,
+  initialValue?: InitialValue,
+): Result;
+declare function reduceRight<Type, Result extends any = any, InitialValue extends any = any>(
+  iterable: ArrayIterable<Type>,
+  handler: ArrayReduceHandler<Type, Result>,
+  initialValue?: InitialValue,
+): Result;
 
-declare function some(iterable: ArrayIterable, handler: ArrayHandler): boolean;
-declare function someObject(iterable: ObjectIterable, handler: ObjectHandler): boolean;
-declare function someRight(iterable: ArrayIterable, handler: ArrayHandler): boolean;
+declare function some<Type>(iterable: ArrayIterable<Type>, handler: ArrayHandler<Type>): boolean;
+declare function someObject<Type>(
+  iterable: ObjectIterable<Type>,
+  handler: ObjectHandler<Type>,
+): boolean;
+declare function someRight<Type>(
+  iterable: ArrayIterable<Type>,
+  handler: ArrayHandler<Type>,
+): boolean;
 
 export {
   every,
