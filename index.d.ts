6b79c928b611c7bab73cb7ff1a9e8bfaf217af4e
Tony Quetano <tony.quetano@planttheidea.com>
2023-12-18 13:28:46 -0500

    support readonly arrays / objects

--- index.d.ts
+++ index.d.ts
@@ -25,109 +25,125 @@ type ObjectReduceHandler<Item, Result> = (
 ) => Result;
 
 declare function every<Item>(
-  collection: Item[],
+  collection: Item[] | readonly Item[],
   handler: ArrayHandler<Item, unknown>,
 ): boolean;
 declare function everyObject<Item>(
-  collection: Record<number | string, Item>,
+  collection:
+    | Record<number | string, Item>
+    | Readonly<Record<number | string, Item>>,
   handler: ObjectHandler<Item, unknown>,
 ): boolean;
 declare function everyRight<Item>(
-  collection: Item[],
+  collection: Item[] | readonly Item[],
   handler: ArrayHandler<Item, unknown>,
 ): boolean;
 
 declare function filter<Item>(
-  collection: Item[],
+  collection: Item[] | readonly Item[],
   handler: ArrayHandler<Item, unknown>,
 ): Item[];
 declare function filterObject<Item>(
-  collection: Record<number | string, Item>,
+  collection:
+    | Record<number | string, Item>
+    | Readonly<Record<number | string, Item>>,
   handler: ObjectHandler<Item, unknown>,
 ): Record<number | string, Item>;
 declare function filterRight<Item>(
-  collection: Item[],
+  collection: Item[] | readonly Item[],
   handler: ArrayHandler<Item, unknown>,
 ): Item[];
 
 declare function find<Item>(
-  collection: Item[],
+  collection: Item[] | readonly Item[],
   handler: ArrayHandler<Item, unknown>,
 ): Item | undefined;
 declare function findObject<Item>(
-  collection: Record<number | string, Item>,
+  collection:
+    | Record<number | string, Item>
+    | Readonly<Record<number | string, Item>>,
   handler: ObjectHandler<Item, unknown>,
 ): Item | undefined;
 declare function findLast<Item>(
-  collection: Item[],
+  collection: Item[] | readonly Item[],
   handler: ArrayHandler<Item, unknown>,
 ): Item | undefined;
 
 declare function findIndex<Item>(
-  collection: Item[],
+  collection: Item[] | readonly Item[],
   handler: ArrayHandler<Item, unknown>,
 ): number;
 declare function findKey<Item>(
-  collection: Record<number | string, Item>,
+  collection:
+    | Record<number | string, Item>
+    | Readonly<Record<number | string, Item>>,
   handler: ObjectHandler<Item, unknown>,
 ): string | void;
 declare function findLastIndex<Item>(
-  collection: Item[],
+  collection: Item[] | readonly Item[],
   handler: ArrayHandler<Item, unknown>,
 ): number;
 
 declare function forEach<Item>(
-  collection: Item[],
+  collection: Item[] | readonly Item[],
   handler: ArrayHandler<Item, void>,
 ): void;
 declare function forEachObject<Item>(
-  collection: Record<number | string, Item>,
+  collection:
+    | Record<number | string, Item>
+    | Readonly<Record<number | string, Item>>,
   handler: ObjectHandler<Item, void>,
 ): void;
 declare function forEachRight<Item>(
-  collection: Item[],
+  collection: Item[] | readonly Item[],
   handler: ArrayHandler<Item, void>,
 ): void;
 
 declare function map<Item, Result>(
-  collection: Item[],
+  collection: Item[] | readonly Item[],
   handler: ArrayHandler<Item, Result>,
 ): Result[];
 declare function mapObject<Item, Result>(
-  collection: Record<number | string, Item>,
+  collection:
+    | Record<number | string, Item>
+    | Readonly<Record<number | string, Item>>,
   handler: ObjectHandler<Item, Result>,
 ): Record<number | string, Result>;
 declare function mapRight<Item, Result>(
-  collection: Item[],
+  collection: Item[] | readonly Item[],
   handler: ArrayHandler<Item, Result>,
 ): Result[];
 
 declare function reduce<Item, Result>(
-  collection: Item[],
+  collection: Item[] | readonly Item[],
   handler: ArrayReduceHandler<Item, Result>,
   initialValue?: Result,
 ): Result;
 declare function reduceObject<Item, Result>(
-  collection: Record<number | string, Item>,
+  collection:
+    | Record<number | string, Item>
+    | Readonly<Record<number | string, Item>>,
   handler: ObjectReduceHandler<Item, Result>,
   initialValue?: Result,
 ): Result;
 declare function reduceRight<Item, Result>(
-  collection: Item[],
+  collection: Item[] | readonly Item[],
   handler: ArrayReduceHandler<Item, Result>,
   initialValue?: Result,
 ): Result;
 
 declare function some<Item>(
-  collection: Item[],
+  collection: Item[] | readonly Item[],
   handler: ArrayHandler<Item, unknown>,
 ): boolean;
 declare function someObject<Item>(
-  collection: Record<number | string, Item>,
+  collection:
+    | Record<number | string, Item>
+    | Readonly<Record<number | string, Item>>,
   handler: ObjectHandler<Item, unknown>,
 ): boolean;
 declare function someRight<Item>(
-  collection: Item[],
+  collection: Item[] | readonly Item[],
   handler: ArrayHandler<Item, unknown>,
 ): boolean;
 
