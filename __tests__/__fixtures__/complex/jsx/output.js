a6cc8829243564a129de28d5db8fcc7ba5a1a300
Tony Quetano <tony.quetano@planttheidea.com>
2023-12-18 13:04:31 -0500

    reuse member expressions as the local collection (#35)

--- __tests__/__fixtures__/complex/jsx/output.js
+++ __tests__/__fixtures__/complex/jsx/output.js
@@ -1,23 +1,16 @@
 import React from 'react';
 function List(props) {
-  return /*#__PURE__*/ React.createElement(
-    'ul',
-    null,
-    (() => {
-      const _collection = props.items;
-      const _length = _collection.length;
-      const _results = Array(_length);
-      for (let _key = 0, _item; _key < _length; ++_key) {
-        _item = _collection[_key];
-        _results[_key] = /*#__PURE__*/ React.createElement(
-          'li',
-          {
-            key: _item.id,
-          },
-          _item.value,
-        );
-      }
-      return _results;
-    })(),
-  );
+  const _length = props.items.length;
+  const _results = Array(_length);
+  for (let _key = 0, _item; _key < _length; ++_key) {
+    _item = props.items[_key];
+    _results[_key] = /*#__PURE__*/ React.createElement(
+      'li',
+      {
+        key: _item.id,
+      },
+      _item.value,
+    );
+  }
+  return /*#__PURE__*/ React.createElement('ul', null, _results);
 }
